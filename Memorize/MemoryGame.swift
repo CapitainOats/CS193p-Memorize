6cdfcbcb873840434857c551bd1a7ec4a946a4d1
tomas.havlicek <tomas.havlicek@avast.com>
2020-07-21 18:01:12 +0200

    Lecture 2: : MVVM and the Swift Type System

---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Tomas Havlicek on 21/07/2020.
+//  Copyright © 2020 Tomas Havlicek. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        print("card choosen: \(card)")
+    }
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = []
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = true
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
